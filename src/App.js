0bb8449b73b15bf4cdfaa5e2ec6c3054c9777525
Brian Hopkins <brianhh1230@gmail.com>
2018-02-23 07:58:52 -0500

    changing from floating

--- src/App.js
+++ src/App.js
@@ -24,7 +24,6 @@ class App extends Component {
                   headerTitle="Mycroft Helper"
                   placeholder="Type away..."
                   customDelay="500"
-                  floating="true"
                 />
               </ThemeProvider>
             </div>
--- src/components/Install.js
+++ src/components/Install.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-var Linkify = require('react-linkify');
 
 
 class Install extends Component {
